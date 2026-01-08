6b3e71b3cbdcd25af946e00edeaad1ceebff0f1d
Hiro Ito <mobiiii24@gmail.com>
2026-01-09 05:18:44 +0900

    feat: エラーページ、ポリシー、フッター追加

--- trail_status/urls.py
+++ trail_status/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from . import views
 
@@ -8,4 +9,6 @@ urlpatterns = [
     # 詳細画面: /1/ や /2/ など
     # <int:pk> 部分が views.py の pk という引数に渡されます
     path("<int:pk>/", views.condition_detail, name="condition-detail"),
+    # プライバシーポリシー
+    path("privacy/", TemplateView.as_view(template_name="privacy.html"), name="privacy"),
 ]
