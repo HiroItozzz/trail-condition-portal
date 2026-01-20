dcd465724dd20853031e77d6386f83b6eeffd4a1
Hiro Ito <mobiiii24@gmail.com>
2026-01-20 21:12:10 +0900

    refactor: js

---
+++ static/js/datatable-ui.js
@@ -0,0 +1,14 @@
+$("#trail-table tbody").on("click", "tr", function (e) {
+  if ($(e.target).closest("a").length) return;
+
+  if (window.innerWidth >= 1442) {
+    var detailUrl = $(this).find("td[data-detail-url]").data("detail-url");
+    if (detailUrl) {
+      window.location.href = detailUrl;
+    }
+  }
+});
+
+if (window.innerWidth >= 1442) {
+  $("#trail-table tbody tr").css("cursor", "pointer");
+}
