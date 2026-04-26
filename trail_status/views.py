65552ba5add475b49c5aa2ed4ab4eebef1f4f14e
Hiro Ito <mobiiii24@gmail.com>
2026-04-26 13:50:56 +0900

    refactor: トップ、情報源ページビュー簡略化

--- trail_status/views.py
+++ trail_status/views.py
@@ -1,10 +1,12 @@
 import logging
 from datetime import timedelta
+from typing import override
 
 from django.views.generic import ListView, DetailView
-from django.db.models import Count, F, Max
+from django.db.models import Count, F, Max, Q
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, redirect, render
+from collections import defaultdict
 from django.utils import timezone
 
 from .models import AreaName, BlogFeed, DataSource, StatusType, TrailCondition
@@ -19,6 +21,7 @@ logger = logging.getLogger(__name__)
 class SideBarMixin:
     """サイドバー用のフィルター選択肢を取得（データがある項目のみ表示）"""
 
+    @override
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context.update(**_get_sidebar_context())
@@ -31,12 +34,14 @@ class TrailListView(SideBarMixin, ListView):
     queryset = TrailCondition.objects.filter(disabled=False).prefetch_related("source")
     template_name = "trail_list.html"
 
+    @override
     def setup(self, request, *args, **kwargs):
         super().setup(request, *args, **kwargs)
         self.source_filter = request.GET.get("source")
         self.area_filter = request.GET.get("area")
         self.status_filter = request.GET.get("status")
 
+    @override
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
@@ -62,22 +67,21 @@ class TrailListView(SideBarMixin, ListView):
 
         # 1週間以内の更新リスト（新規追加情報源は除外）
         # 新規追加情報源 = DataSource.created_atとTrailCondition.updated_atの差が1日以内
+        seven_days_ago = timezone.now().date() - timedelta(days=7)
         # @formatter:off
-        updated_sources_query = (self.get_queryset()
-                                     .values("source__name", "source__url1")
-                                     .annotate(
-                                         latest_date=Max("updated_at"),
-                                        source_created_at=F("source__created_at"),
-                                     ).order_by("-latest_date")
+        context["recent_updated_sources"] = (self.get_queryset()
+                                   .values("source__name", "source__url1")
+                                   .annotate(
+                                       latest_date=Max("updated_at"),
+                                       source_created_at=F("source__created_at"),
+                                   # DataSourceの作成日とTrailConditionの最新更新日の差が1日以内のものを除外
+                                   ).filter(latest_date__gt=F("source_created_at") + timedelta(days=1))
+                                   .filter(latest_date__date__gte=seven_days_ago)
+                                   .order_by("-latest_date")
                                 )
         # @formatter:on
-        # DataSourceの作成日とTrailConditionの最新更新日の差が1日以内のものを除外
-        context["updated_sources"] = [
-            item for item in updated_sources_query if
-            item["latest_date"] - item["source_created_at"] > timedelta(days=1)
-        ]
+
         context["last_checked_at"] = datasource.aggregate(Max("last_checked_at"))["last_checked_at__max"]
-        context["seven_days_ago"] = timezone.now().date() - timedelta(days=7)
 
         return context
 
@@ -88,6 +92,7 @@ class TrailDetailView(SideBarMixin, DetailView):
     template_name = "detail.html"
     context_object_name = "item"
 
+    @override
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
@@ -111,7 +116,17 @@ class SourceListView(SideBarMixin, ListView):
     model = DataSource
     queryset = DataSource.objects.filter(data_format="WEB").order_by("organization_type", "id")
     template_name = "sources.html"
-    context_object_name = "sources"
+
+    @override
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        sources_by_type = defaultdict(list)
+
+        for source in self.get_queryset():
+            sources_by_type[source.organization_type].append(source)
+        context["sources_by_type"] = dict(sources_by_type)  # Not work in template if defaultdict
+
+        return context
 
 
 class BlogListView(SideBarMixin, ListView):
@@ -119,20 +134,22 @@ class BlogListView(SideBarMixin, ListView):
     model = DataSource
     template_name = "blogs.html"
 
+    @override
     def get_queryset(self):
         # @formatter:off
         queryset = (DataSource.objects.filter(data_format="BLOG")
                      .prefetch_related(
-            Prefetch(
-                "blogfeed_set",
-                queryset=BlogFeed.objects.filter(disabled=False).order_by("-published_at")[:4],
-                to_attr="recent_feeds",
-            )
-        ).order_by("area_name", "id")
-                     )
+                         Prefetch(
+                             "blogfeed_set",
+                             queryset=BlogFeed.objects.filter(disabled=False).order_by("-published_at")[:4],
+                             to_attr="recent_feeds",
+                         )
+                     ).order_by("area_name", "id")
+                   )
         # @formatter:on
         return queryset
 
+    @override
     def get_context_data(self, **kwargs):
 
         context = super().get_context_data(**kwargs)
